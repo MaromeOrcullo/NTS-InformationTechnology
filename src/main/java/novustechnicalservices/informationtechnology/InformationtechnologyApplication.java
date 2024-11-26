093027493c6f98c323f859ffeb59ccb84e9d0510
MaromeOrcullo <marome.orcullo@novustechserv.com>
2024-11-26 13:42:10 -0700

    First Commit

---
+++ src/main/java/novustechnicalservices/informationtechnology/InformationtechnologyApplication.java
@@ -0,0 +1,13 @@
+package novustechnicalservices.informationtechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationtechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationtechnologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novustechnicalservices/informationtechnology/InformationtechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package novustechnicalservices.informationtechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationtechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
